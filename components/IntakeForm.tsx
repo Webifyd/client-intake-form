fbdd43f62be8e9bd580619d2a305bd8235aebf92
shafeer-dev <shafeer@webifyd.com>
2025-11-18 11:57:24 +0400

    fix: resolve hydration error by using span instead of div in Tooltip component

--- components/IntakeForm.tsx
+++ components/IntakeForm.tsx
@@ -9,7 +9,7 @@ function Tooltip({ text }: { text: string }) {
   const [show, setShow] = useState(false);
 
   return (
-    <div className="relative inline-block ml-1">
+    <span className="relative inline-block ml-1">
       <button
         type="button"
         className="text-webifyd-blue hover:text-webifyd-navy transition-colors"
@@ -22,14 +22,14 @@ function Tooltip({ text }: { text: string }) {
         </svg>
       </button>
       {show && (
-        <div className="absolute z-10 w-64 p-2 text-xs text-white bg-gray-900 rounded-lg shadow-lg bottom-full left-1/2 transform -translate-x-1/2 mb-2">
+        <span className="absolute z-10 w-64 p-2 text-xs text-white bg-gray-900 rounded-lg shadow-lg bottom-full left-1/2 transform -translate-x-1/2 mb-2 block">
           {text}
-          <div className="absolute top-full left-1/2 transform -translate-x-1/2 -mt-1">
-            <div className="border-4 border-transparent border-t-gray-900"></div>
-          </div>
-        </div>
+          <span className="absolute top-full left-1/2 transform -translate-x-1/2 -mt-1 block">
+            <span className="border-4 border-transparent border-t-gray-900 block"></span>
+          </span>
+        </span>
       )}
-    </div>
+    </span>
   );
 }
 
